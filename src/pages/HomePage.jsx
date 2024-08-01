2c03bdb0b4200061df781ca8647508996e04b296
Gonzalo Rodriguez <gonzzalorodriguez13@gmail.com>
2024-08-01 15:11:06 -0300

    mejorando el rendimiento y el SEO

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,10 +6,26 @@ import Features from "../components/features/Features";
 import Footer from "../components/Footer";
 import Closing from "../components/closing/Closing";
 import { Box } from "@mui/material";
+import { Helmet } from "react-helmet";
 
 const HomePage = () => {
   return (
     <Box sx={{ overflowX: "hidden" }}>
+      <Helmet>
+        <title>Angelina | Nutrición y ejercicio</title>
+        <meta
+          name="description"
+          content="Tienda online de Angelina Rodríguez dedicada a la venta de planes nutricionales y de ejercicio. Conocé las ventajas de una vida saludable y las caracteristicas de mis planes"
+        />
+        <meta
+          name="keywords"
+          content="Angelina, Angelina Rodríguez, nutrición, ejercicio, planes, planes nutricionales, planes de ejercicio, fitness, vida saludable"
+        />
+        <link
+          rel="canonical"
+          href="https://angelina-nutritionandworkout.netlify.app/"
+        />
+      </Helmet>
       <Header />
       <HeroSection />
       <Benefits />
--- src/pages/PlansPage.jsx
+++ src/pages/PlansPage.jsx
@@ -4,10 +4,43 @@ import Products from "../components/products/Products";
 import Footer from "../components/Footer";
 import NutritionalCover from "../components/products/NutritionalCover";
 import WorkoutCover from "../components/products/WorkoutCover";
+import { Helmet } from "react-helmet";
 
 const PlansPage = ({ category }) => {
+  const getHelmetContent = () => {
+    if (category === 2) {
+      return {
+        title: "Productos | Nutrición",
+        description:
+          "Planes nutricionales de la mejor calidad y al mejor precio",
+        keywords:
+          "Angelina, nutrición, planes, planes nutricionales, planes nutricionales personalizados, dieta saludable, comer sano, comida fitness",
+        canonical:
+          "https://angelina-nutritionandworkout.netlify.app/nutritional-plans",
+      };
+    } else if (category === 1) {
+      return {
+        title: "Productos | Ejercicio",
+        description:
+          "Planes de ejercicio de la mejor calidad y al mejor precio, para que alcances tu mejor versión física",
+        keywords:
+          "Angelina, ejercicio, planes, planes de ejercicio, planes de musculación, rutinas de ejercicio, entrenamiento físico, cambio corporal",
+        canonical:
+          "https://angelina-nutritionandworkout.netlify.app/muscle-plans",
+      };
+    }
+  };
+
+  const helmetContent = getHelmetContent();
+
   return (
     <>
+      <Helmet>
+        <title>{helmetContent.title}</title>
+        <meta name="description" content={helmetContent.description} />
+        <meta name="keywords" content={helmetContent.keywords} />
+        <link rel="canonical" href={helmetContent.canonical} />
+      </Helmet>
       <Header />
       {category === 2 && <NutritionalCover />}
       {category === 1 && <WorkoutCover />}
